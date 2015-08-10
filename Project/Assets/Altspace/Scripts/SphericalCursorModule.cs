2d15d6c7a41ff2e976c076473b42dc8885654778
hqtay <tayhuiqithq@gmail.com>
2015-08-09 19:14:01 -0700

    Added Spherical Coordinates calculation from first principles

--- Project/Assets/Altspace/Scripts/SphericalCursorModule.cs
+++ Project/Assets/Altspace/Scripts/SphericalCursorModule.cs
@@ -43,19 +43,16 @@ public class SphericalCursorModule : MonoBehaviour {
 		Vector3 direction = ray.direction.normalized;
 		cursor.transform.position = transform.position + direction * SphereRadius;
 
-		/* Spherical Coordinates Calculation From First Principles
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		
+		/* Spherical Coordinates Calculation From First Principles */	
 		// Find the polar and azimuth angles from this ray direction
-		float polar = Vector3.Angle(Vector3.up, ray.direction); // polar won't go beyond 180 degrees
-		float azimuth = Vector3.Angle(Vector3.forward, ray.direction); // azimuth need to calculate in 360 degrees
-		float sign = Mathf.Sign(Vector3.Dot(Vector3.up, Vector3.Cross(Vector3.forward, ray.direction)));
-		azimuth = sign * azimuth;
-
-		//Debug.DrawRay(ray.origin, ray.direction);
-		MouseHandler mh = new MouseHandler(SphereRadius, azimuth, polar);
-		cursor.transform.position = transform.position + mh.SphericalToCartesian();
-		 */
+//		float polar = Vector3.Angle(Vector3.up, direction); // polar won't go beyond 180 degrees
+//		Vector3 directionAzimuth = new Vector3(direction.x, 0f, direction.z);
+//		float azimuth = Vector3.Angle(Vector3.forward, directionAzimuth); // azimuth need to calculate in 360 degrees
+//		float sign = Mathf.Sign(Vector3.Dot(Vector3.up, Vector3.Cross(Vector3.forward, directionAzimuth)));
+//		azimuth = sign * azimuth;
+//		// Update Mouse Handler
+//		MouseHandler.Instance.UpdateSphericalCoordinates(SphereRadius, azimuth, polar);
+//		cursor.transform.position = transform.position + MouseHandler.Instance.SphericalToCartesian();
 
 		// TODO: Perform ray cast to find object cursor is pointing at.
 		// TODO: Update cursor transform.
@@ -65,7 +62,6 @@ public class SphericalCursorModule : MonoBehaviour {
 		// Update highlighted object based upon the raycast.
 		if (cursorHit.collider != null)
 		{
-			//Debug.Log("Hit furniture");
 			Selectable.CurrentSelection = cursorHit.collider.gameObject;
 
 			// Modify the cursor size based on distance
@@ -75,38 +71,8 @@ public class SphericalCursorModule : MonoBehaviour {
 		}
 		else
 		{
-			//Debug.Log("Did Not Hit furniture");
 			Selectable.CurrentSelection = null;
 			cursor.transform.localScale = DefaultCursorScale;
 		}
 	}
-}
-
-/// <summary>
-/// Class for handling mouse spherical calculations.
-/// </summary>
-public class MouseHandler {
-	public float r; // radial distance
-	public float azimuth; // azimuthal angle
-	public float polar; // polar angle
-
-	public MouseHandler(float _radialDist, float _azimuthalAngle, float _polarAngle) {
-		r = _radialDist;
-		azimuth = _azimuthalAngle;
-		polar = _polarAngle;
-	}
-
-	public Vector3 SphericalToCartesian() {
-		// Convert to radians
-		float polarR = polar * Mathf.Deg2Rad;
-		float azimuthR = azimuth * Mathf.Deg2Rad;
-
-		// Find cartesian coordinates
-		float x = r * Mathf.Sin(polarR) * Mathf.Cos(azimuthR);
-		float y = r * Mathf.Cos(polarR);
-		float z = r * Mathf.Sin(polarR) * Mathf.Sin(azimuthR);
-		
-		return new Vector3(x, y, z);
-	}
-
 }
---
+++ Project/Assets/Scripts/MouseHandler.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Class for handling mouse spherical calculations.
+/// </summary>
+public class MouseHandler {
+	private static MouseHandler instance;
+	public float r; // radial distance
+	public float azimuth; // azimuthal angle
+	public float polar; // polar angle
+
+	/// <summary>
+	/// Singleton. Gets the instance.
+	/// </summary>
+	public static MouseHandler Instance
+	{
+		get {
+			if (instance == null)
+			{
+				instance = new MouseHandler();
+			}
+			return instance;
+		}
+	}
+
+	public void UpdateSphericalCoordinates(float _radialDist, float _azimuthalAngle, float _polarAngle) {
+		r = _radialDist;
+		azimuth = _azimuthalAngle;
+		polar = _polarAngle;
+	}
+	
+	public Vector3 SphericalToCartesian() {
+		// Convert to radians
+		float polarR = polar * Mathf.Deg2Rad;
+		float azimuthR = azimuth * Mathf.Deg2Rad;
+		
+		// Find cartesian coordinates
+		float x = r * Mathf.Sin(polarR) * Mathf.Sin(azimuthR);
+		float y = r * Mathf.Cos(polarR);
+		float z = r * Mathf.Sin(polarR) * Mathf.Cos(azimuthR);
+		
+		return new Vector3(x, y, z);
+	}
+	
+}
